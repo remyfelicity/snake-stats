022da9e7e33d480d1d0bf93245c9f4bab8732053
remyfelicity <99709675+remyfelicity@users.noreply.github.com>
2025-12-23 15:19:30 -0800

    Add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
-
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()],
   },
