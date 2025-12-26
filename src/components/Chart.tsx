31e9991e5b52754cb4c1d61a3a93c79f5b17fc00
remyfelicity <99709675+remyfelicity@users.noreply.github.com>
2025-12-26 13:00:40 -0800

    Decompose App component into subcomponents

---
+++ src/components/Chart.tsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+  type TooltipContentProps,
+} from "recharts";
+import { type ChartData } from "../lib/chart-data";
+
+const LINE_COLORS = [
+  "oklch(62.3% 0.214 259.815)",
+  "oklch(63.7% 0.237 25.331)",
+  "oklch(72.3% 0.219 149.579)",
+  "oklch(66.7% 0.295 322.15)",
+  "oklch(79.5% 0.184 86.047)",
+  "oklch(71.5% 0.143 215.221)",
+] as const;
+
+function CustomTooltip({
+  active,
+  payload,
+  label,
+}: TooltipContentProps<string | number, string>) {
+  if (!active || !payload || !payload.length) {
+    return null;
+  }
+
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "medium",
+  }).format(new Date(label!));
+  return (
+    <div className="rounded border border-gray-300 bg-white p-2">
+      <p className="font-medium">{formattedDate}</p>
+      {payload.map((entry) => {
+        const formattedDownloadCount = new Intl.NumberFormat("en-US").format(
+          entry.value,
+        );
+        return (
+          <p className="flex gap-1" key={entry.name}>
+            <span style={{ color: entry.stroke }}>{entry.name}</span>
+            {formattedDownloadCount}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
+export function Chart({
+  data,
+  packages,
+}: {
+  data: ChartData;
+  packages: string[];
+}) {
+  const [range, setRange] = useState(180);
+  const dataSlice = data.slice(-range);
+
+  if (packages.length === 0) return null;
+  return (
+    <>
+      <div className="mt-8">
+        <label className="text-lg" htmlFor="range">
+          Downloads in the past
+        </label>
+        <select
+          className="ml-1 text-lg"
+          id="range"
+          name="range"
+          onChange={(event) => setRange(Number(event.target.value))}
+          value={range}
+        >
+          <option value={30}>30 days</option>
+          <option value={90}>90 days</option>
+          <option value={180}>180 days</option>
+        </select>
+      </div>
+      <LineChart
+        className="mt-4 h-96 w-full md:h-128"
+        data={dataSlice}
+        responsive
+      >
+        <CartesianGrid />
+        <Legend />
+        <Tooltip content={CustomTooltip} />
+        <XAxis
+          dataKey="_date"
+          domain={["dataMin", "dataMax"]}
+          tickFormatter={(date) =>
+            new Intl.DateTimeFormat("en-US", {
+              day: "2-digit",
+              month: "short",
+            }).format(new Date(date))
+          }
+          type="number"
+        />
+        <YAxis
+          tickFormatter={(downloadCount) =>
+            new Intl.NumberFormat("en-US", {
+              compactDisplay: "short",
+              notation: "compact",
+            }).format(downloadCount)
+          }
+          width={48}
+        />
+        {packages.map((package_, i) => (
+          <Line
+            dataKey={package_}
+            dot={false}
+            key={package_}
+            name={package_}
+            stroke={LINE_COLORS[i]}
+            strokeWidth="2"
+            type="monotone"
+          />
+        ))}
+      </LineChart>
+    </>
+  );
+}
---
+++ src/components/Input.tsx
@@ -0,0 +1,62 @@
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { navigate } from "astro:transitions/client";
+import { useState, type FormEvent } from "react";
+
+const MAX_PACKAGES = 6;
+
+function updateRoute(packages: string[]) {
+  const slug = packages.join("+");
+  const url = slug ? `/${slug}` : "/";
+  navigate(url);
+}
+
+export function Input({ packages }: { packages: string[] }) {
+  const [input, setInput] = useState("");
+
+  async function handleAddPackage(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const trimmedInput = input.trim();
+
+    if (!trimmedInput) return;
+    if (packages.includes(trimmedInput)) return;
+    if (packages.length >= MAX_PACKAGES) return;
+
+    updateRoute([...packages, trimmedInput].toSorted());
+    setInput("");
+  }
+
+  function handleRemovePackage(packageToRemove: string) {
+    updateRoute(packages.filter((package_) => package_ !== packageToRemove));
+  }
+
+  return (
+    <>
+      <form onSubmit={(event) => void handleAddPackage(event)}>
+        <input
+          className="mt-4 h-12 w-full rounded-full border border-gray-300 px-6 placeholder:text-gray-500"
+          onChange={(event) => setInput(event.target.value)}
+          placeholder="Enter a PyPI package..."
+          value={input}
+        />
+      </form>
+      <div className="mt-4 flex flex-wrap gap-2">
+        {packages.map((package_) => (
+          <div
+            className="flex h-12 items-center rounded-full border border-gray-300 pl-4 text-gray-700"
+            key={package_}
+          >
+            {package_}
+            <button
+              className="grid size-12 cursor-pointer place-items-center rounded-full"
+              onClick={() => handleRemovePackage(package_)}
+            >
+              <XMarkIcon className="size-5 text-gray-500" />
+              <span className="sr-only">Remove</span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
